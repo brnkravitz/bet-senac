57af26e244df4eca29d8f714735c9f07f70a78cf
Bruno Nunes <bnsb2007@gmail.com>
2024-11-21 20:22:30 -0300

    mudando para sqlite3

--- banco.js
+++ banco.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
         dialect: 'sqlite',
-        sotage: './database.sqlite'
+        storage: './database.sqlite'
     }
 )
 
